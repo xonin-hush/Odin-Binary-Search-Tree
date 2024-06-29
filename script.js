363f119b9db28124b9f5d65fee87defa407a1643
xonin-hush <xonindev@gmail.com>
2024-06-29 16:55:02 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+class Node{
+    constructor(data){
+        this.data=data
+        this.leftChild=null
+        this.rightChild=null
+    }
+}
+class Tree{
+    constructor(root){
+        this.root=root
+    }
+}
