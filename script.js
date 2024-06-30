3b8fa8d19e73d8dd7cc2cf50bbaf1357ef8305a2
Xonin-hush <xonindev@gmail.com>
2024-06-30 09:42:02 +0300

    Add buildtree() & prettyprint()

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-class Node{
-    constructor(data){
-        this.data=data
-        this.leftChild=null
-        this.rightChild=null
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
 }
-class Tree{
-    constructor(root){
-        this.root=root
-    }
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array, 0, array.length - 1);
+  }
+  buildTree(array, start, end) {
+    if (start > end) return null;
+
+    let mid = Math.floor((start + end) / 2);
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+
+    return root;
+  }
 }
+L = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+console.log(L);
+prettyPrint(L.root)
